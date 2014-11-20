0b7e160ef3acba907a20b49ab2dfbcdffab07bf6
Austen Higgins-Cassidy <plasmarobo@gmail.com>
2014-11-20 18:01:28 +0000

    Fixed accident, stop pushing directly to master!

--- test/models/prerequsite_test.rb
+++ test/models/prerequsite_test.rb
@@ -4,4 +4,15 @@ class PrerequsiteTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "Prereq Level" do
+    assert false
+  end
+  
+  test "Prereq Attribute" do
+    assert false
+  end
+  
+  test "Prereq Skill" do
+    assert false
+  end
 end
